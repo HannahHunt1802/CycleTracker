1a265562d350df3e0f1e5ce3e3781feba2ae6086
HannahHunt1802 <hannahhunt5656@gmail.com>
2025-11-23 18:41:43 +0000

    feat: added hashed password support to User database

--- app/models.py
+++ app/models.py
@@ -1,12 +1,20 @@
 from app import db
 from sqlalchemy import DateTime, func
 from flask_login import UserMixin
+from flask_bcrypt import Bcrypt
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False) #length to store hashed passwords
+
+    # store passwords securely
+    def set_password(self, raw_password):
+        self.password = Bcrypt.generate_password_hash(raw_password).decode('utf-8')
+
+    def check_password(self, raw_password):
+        return Bcrypt.check_password_hash(self.password, raw_password)
 
     #optional info
     dob = db.Column(db.Date, nullable=True)
